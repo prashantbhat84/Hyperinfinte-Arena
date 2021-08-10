8f139ca6112a71834de758bded394a07d6854f76
Prashant Bhat <prashantbhat84@gmail.com>
2021-08-10 16:51:48 +0530

    inital set up

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { CssBaseline, Typography, } from '@material-ui/core'
+
+function App() {
+    return (
+        <>
+            <CssBaseline />
+            <Typography variant="h3">Let's get Started</Typography>
+        </>
+    )
+}
+
+export default App
